ef4313e6101e3d5b929e5210e1e02f74c10b5d45
Vladyslav Tykhyi <vladleonidovich01@gmail.com>
2024-12-02 19:17:44 +0100

    almost done

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,57 +1,30 @@
-// У файлі pixabay-api.js зберігай функції для HTTP-запитів
+// const API_URL ='https://pixabay.com/api/'
+// const API_KEY ='47362055-4c26e776b4d447c79837e3674'
 
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
+// const input = document.querySelector(".form-input")
+// export const params = new URLSearchParams({
+//   key: API_KEY,
+//   q: input.value
+// })
+
+const API_KEY = '47379272-a961c7172d29abe92af06f616';
+
+export const params = new URLSearchParams({
+  key: API_KEY,
+  q: 'yellow+flower',
+});
+
+const BASE_URL = `https://pixabay.com/api/?${params}`;
+
+// const base_Url = `${API_URL}?${params}`
 
 
-const API_URL ='https://pixabay.com/api/'
 
-const API_KEY ='47362055-4c26e776b4d447c79837e3674'
-
-const form = document.querySelector(".form")
-const btn = document.querySelector(".btn-js")
-const input = document.querySelector(".form-input")
-
-
-
-export const btnEvent = btn.addEventListener("click", searchClick)
-
-function searchClick(event){
-  event.preventDefault()
-
-  if (!input.value.trim()) {
-    iziToast.show({
-      message: "Input cannot be empty. Please enter a search term!"
-    });
-    return;
-  }
-  
-  const params = new URLSearchParams({
-    key: API_KEY,
-    q: input.value
-  })
-  // console.log(params.toString());
-  
-  fetch(`${API_URL}?${params}`)
-    .then(response => {
-    if(!response.ok){
-      throw new Error("Sorry, there are no images matching your search query. Please try again!")
+export function fetchRequest(url = BASE_URL) {
+  return fetch(url).then(response => {
+    if (!response.ok) {
+      throw new Error("Sorry, no results found for your query. Please try another search term.");
     }
-    return response.json()
-  })
-    .then(data =>{
-      if (data.hits.length === 0) {
-        throw new Error("Sorry, no results found for your query. Please try another search term.");
-      }
-      console.log(data);
-    }
-    )
-    .catch(err => {
-      iziToast.show({
-            message: `${err.message}`
-        });
-    })
-    .finally(() => {
-      input.value = ""
-    })
+    return response.json();
+  });
 }
